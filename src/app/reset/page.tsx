94a35f689ac5151c22e0fbe5730354df52e1bcde
formysister <enemymaycry2007@gmail.com>
2025-07-05 00:46:41 +0900

    ready to deploy

--- src/app/reset/page.tsx
+++ src/app/reset/page.tsx
@@ -1,9 +1,12 @@
+import React, { Suspense } from "react"
 import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
 
 export default function LoginPage() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-blue-900 to-blue-800">
-            <ResetPasswordForm />
+            <Suspense>
+                <ResetPasswordForm />
+            </Suspense>
         </div>
     );
 }
